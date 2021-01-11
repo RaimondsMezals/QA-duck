acac1a58838bce933291b787e01e844ee32a72cd
RaimondsMezalsQA <RMezals@qa.com>
2021-01-11 17:23:16 +0000

    duck finished task 3

---
+++ src/com/qa/main/Duck.java
@@ -0,0 +1,100 @@
+package com.qa.main;
+
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ArrayList;
+
+public class Duck {
+	
+	public List<Duck> duckList = new ArrayList<>();
+	
+	private String name;
+	private boolean isAMallard;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public boolean isAMallard() {
+		return isAMallard;
+	}
+	public void setAMallard(boolean isAMallard) {
+		this.isAMallard = isAMallard;
+	}
+	
+	
+	public void setDuckList(Duck input) {
+		
+		duckList.add(input);
+		
+	}
+	 public void getDuckList() {
+		 
+			System.out.println(duckList);
+		 
+	 }
+	 
+	 public void searchDuckList(int input) {
+		 
+		 System.out.println(duckList.get(input));
+		 
+	 }
+	 
+	 public void streamDuckOutput() {
+		 
+		 duckList.stream().forEach( Duck -> System.out.println(Duck));
+		 
+	 }
+	 
+	@Override
+	public String toString() {
+		return "Duck [name=" + name + ", isAMallard=" + isAMallard + "]";
+	}
+	 
+	/*
+	public void searchByMallard(String inputName) {
+		 
+		 
+		 for(Duck i : duckList) {
+			 
+			 String typedDuck = inputName;
+			 boolean currentDuck = i.isAMallard();
+			 
+			 
+			 if(typedDuck == "mallard" && currentDuck == true) {
+				 
+				 System.out.println(i);
+				 
+			 }
+			 
+		 }
+		 
+	 }
+	 */
+	
+	public void printMallard() {
+		 
+		System.out.println("Printing out all mallard ducks!");
+		 
+		 for(Duck i : duckList) {
+			 
+			 boolean currentDuck = i.isAMallard();
+			 
+			 if(currentDuck == true) {
+				 
+				 System.out.println(i);
+				 
+			 }
+			 
+		 }
+		 
+	 }
+	
+	
+	
+
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,55 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Duck duck0 = new Duck();
+		
+		duck0.setName(("Domestic Duck"));
+		duck0.setAMallard(true);
+		
+		Duck duck1 = new Duck();
+		
+		duck1.setName(("Swedish Yellow Duck"));
+		duck1.setAMallard(true);
+		
+		Duck duck2 = new Duck();
+		
+		duck2.setName(("Aylesbury Duck"));
+		duck2.setAMallard(true);
+		
+		Duck duck3 = new Duck();
+		
+		duck3.setName(("American Wigeon Duck"));
+		duck3.setAMallard(false);
+		
+		Duck duck4 = new Duck();
+		
+		duck4.setName(("Northern Pintail Duck"));
+		duck4.setAMallard(false);
+		
+		Duck duck5 = new Duck();
+		
+		duck5.setName(("Hooded Merganser Duck"));
+		duck5.setAMallard(false);
+		
+		Duck dList = new Duck();
+		
+		dList.setDuckList(duck0);
+		dList.setDuckList(duck1);
+		dList.setDuckList(duck2);
+		dList.setDuckList(duck3);
+		dList.setDuckList(duck4);
+		dList.setDuckList(duck5);
+		
+		dList.getDuckList();
+		
+		//dList.streamDuckOutput();
+		
+		dList.printMallard();
+		
+		
+	}
+
+}
